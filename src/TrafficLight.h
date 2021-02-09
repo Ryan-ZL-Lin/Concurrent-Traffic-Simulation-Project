8ca5a6bedb017c0da991de16b36e42854bcb7f58
Ryan-ZL-Lin <lzlallen1980@gmail.com>
2021-02-09 03:01:42 -0800

    second commit after testing

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -9,19 +9,28 @@
 // forward declarations to avoid include cycle
 class Vehicle;
 
-
 // FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
 // Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
 // Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
 // Also, there should be an std::condition_variable as well as an std::mutex as private members. 
+enum TrafficLightPhase
+{
+    red,
+    green
+};
 
 template <class T>
 class MessageQueue
 {
 public:
+    T receive();
+
+    void send(T &&msg);
 
 private:
-    
+    std::condition_variable _condition;
+    std::mutex _mutex;
+    std::deque<TrafficLightPhase> _queue;
 };
 
 // FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
@@ -29,18 +38,22 @@ private:
 // as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that 
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
-
-class TrafficLight
+class TrafficLight : public TrafficObject
 {
 public:
     // constructor / desctructor
+    TrafficLight();
 
     // getters / setters
 
     // typical behaviour methods
+    void waitForGreen();
+    void simulate();
+    TrafficLightPhase getCurrentPhase();
 
 private:
     // typical behaviour methods
+    void cycleThroughPhases();
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
@@ -48,6 +61,8 @@ private:
 
     std::condition_variable _condition;
     std::mutex _mutex;
+    TrafficLightPhase _currentPhase; //identifies the current phase
+    MessageQueue<TrafficLightPhase> _messageQue;
 };
 
 #endif
